dc65ec5ef73de62da5a2f5e86423a605551b1183
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-25 20:33:22 -0800

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 
 
 
 
 const App =() => {
-    return <div>Hi there</div>
+    return (
+
+        <div className="ui container comments" >
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img src={faker.image.avatar()} alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 ReactDOM.render(
