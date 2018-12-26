22d9a3e4aaddb9a16362a5fe6a09589d55c69f2f
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-25 22:25:52 -0800

    used props.children to pastprops to render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommendDetail from './components/comment_detail';
+import ApprovalCard from './components/approval_card';
 
 
 
@@ -9,24 +10,33 @@ const App =() => {
     return (
 
         <div className="ui container comments" >
-            <CommendDetail 
-            author="Sam" 
-            timeAgo="Today at 4:45AM" 
-            img={faker.image.avatar()} 
-            postText="Nice"
-            />
-            <CommendDetail 
-            author="Alex" 
-            timeAgo="Today at 2:00PM" 
-            img={faker.image.avatar()} 
-            postText="Good"
-            />
-            <CommendDetail 
-            author="Jane" 
-            timeAgo="Yesterday at 5:30PM" 
-            img={faker.image.avatar()} 
-            postText="It is oooo" 
-            />
+            <ApprovalCard>
+                <CommendDetail 
+                author="Sam" 
+                timeAgo="Today at 4:45AM" 
+                img={faker.image.avatar()} 
+                postText="Nice"
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommendDetail 
+                author="Alex" 
+                timeAgo="Today at 2:00PM" 
+                img={faker.image.avatar()} 
+                postText="Good"
+                />
+            </ApprovalCard>
+           
+            <ApprovalCard>
+                <CommendDetail 
+                author="Jane" 
+                timeAgo="Yesterday at 5:30PM" 
+                img={faker.image.avatar()} 
+                postText="It is oooo" 
+                />
+            </ApprovalCard>
+            
             
         </div>
     )
